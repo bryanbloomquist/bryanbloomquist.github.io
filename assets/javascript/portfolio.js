1950844e7bf0e351bb7f567b1d54e3256606353b
bloomy215 <bmbloomquist@gmail.com>
2019-12-04 13:48:28 -0600

    added seperate js files for array

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,45 @@
+const portfolio = [
+  {
+    name: "My Little Pony Math Rescue",
+    src: "assets/images/molly-pony.png",
+    alt: "My Little Pony Math Rescue screenshot",
+    skills: "React | Sass | Express.js | Node.js | MongoDB | Bootstrap",
+    deployed: "https://molly-pony.herokuapp.com",
+    github: "https://github.com/bryanbloomquist/molly-pony"
+  },{
+    name: "Choose Your Own Adventure",
+    src: "assets/images/cyoa.png",
+    alt: "Choose Your Own Adventure screenshot",
+    skills: "Bootstrap | JavaScript | jQuery | Node.js | Express.js | mySQL",
+    deployed: "https://cyoa-bc.herokuapp.com",
+    github: "https://github.com/bryanbloomquist/group-project-2"
+  },{
+    name: "Dungeons & Dragons Hangman",
+    src: "assets/images/hangman.png",
+    alt: "Dungeons & Dragons Hangman screenshot",
+    skills: "HTML | CSS | JavaScript | ES6",
+    deployed: "https://bryanbloomquist.github.io/word-guess-game",
+    github: "https://github.com/bryanbloomquist/word-guess-game"
+  },{
+    name: "Google Book Search",
+    src: "assets/images/google-book.png",
+    alt: "Google Book Search screenshot",
+    skills: "MongoDB | Express.js | React | Node.js | Bootstrap | CSS",
+    deployed: "https://react-google.herokuapp.com",
+    github: "https://github.com/bryanbloomquist/google-book-search"
+  },{
+    name: "Gimlet Podcast Scraper",
+    src: "assets/images/gimlet.PNG",
+    alt: "Gimlet Podcast Scraper screenshot",
+    skills: "JavaScript | jQuery | Node.js | Express.js | Cheerio | Axios",
+    deployed: "https://gimlet.herokuapp.com",
+    github: "https://github.com/bryanbloomquist/news-scraper"
+  },{
+    name: "Tarot Reader",
+    src: "assets/images/tarot-reader.png",
+    alt: "Tarot Reader screenshot",
+    skills: "HTML | CSS | JavaScript | jQuery",
+    deployed: "https://bryanbloomquist.github.io/tarot-reader",
+    github: "https://github.com/bryanbloomquist/tarot-reader"
+  }
+];
---
+++ assets/javascript/toolbox.js
@@ -0,0 +1,51 @@
+const toolbox = [
+  {
+    name: "HTML",
+    alt: "HTML logo",
+    src: "assets/images/html.png",
+  },{
+    name: "CSS",
+    alt: "CSS logo",
+    src: "assets/images/css.png",
+  },{
+    name: "JavaScript",
+    alt: "JavaScript logo",
+    src: "assets/images/javascript.png",
+  },{
+    name: "jQuery",
+    alt: "jQuery logo",
+    src: "assets/images/jquery.png",
+  },{
+    name: "React",
+    alt: "React logo",
+    src: "assets/images/react.png",
+  },{
+    name: "Bootstrap",
+    alt: "Bootstrap logo",
+    src: "assets/images/bootstrap.png",
+  },{
+    name: "Sass",
+    alt: "Sass logo",
+    src: "assets/images/sass.png",
+  },{
+    name: "Git",
+    alt: "Git logo",
+    src: "assets/images/git.png",
+  },{
+    name: "Github",
+    alt: "Github logo",
+    src: "assets/images/github.png",
+  },{
+    name: "Node.js",
+    alt: "Node.js logo",
+    src: "assets/images/nodejs.png",
+  },{
+    name: "MongoDB",
+    alt: "Mongo DB logo",
+    src: "assets/images/mongodb.png",
+  },{
+    name: "Express.js",
+    alt: "Express JS logo",
+    src: "assets/images/expressjs.png",
+  }
+];
