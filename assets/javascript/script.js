2494bb7090742b04c9d89d07bef88cd3839af1b8
bloomy215 <bmbloomquist@gmail.com>
2020-02-24 00:01:24 -0600

    restyling page, added contact form

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -27,34 +27,48 @@ const toggel = () => $("#navbar, .navbutton, .content").toggleClass("active");
 /// WELCOMEPAGE ////////////
 const welcomePage = () => {
   $(".greetings").empty();
-  typed(".greetings", "Hi, I'm Bryan.  I design and code awesome websites and web applications.", 0);
+  typed(".greetings", "Hi, I'm Bryan.  I make cool stuff happen on the internet.", 0);
 }
 
 /// PORTFOLIO //////////////////////////////////////////////////
 const portfolioPage = () => {
   for (let i = 0; i < portfolio.length; i++) {
-    $("#portfolio-projects").append(`
-      <div class="project col-12 col-md-6 col-xl-4 mb-3">
-        <div class="project-body text-center">
-          <h5 class="project-title">${portfolio[i].name}</h5>
-          <img class="project-image" src="${portfolio[i].src}" alt="${portfolio[i].alt}" />
-          <p class="project-skills">${portfolio[i].skills}</p>
-          <a class="pagelink btn p-1" href="${portfolio[i].deployed}" target="_blank">Deployed Site</a>
-          <a class="pagelink btn p-1" href="${portfolio[i].github}" target="_blank">Github Repo</a>
-        </div>
-      </div>
-    `);
+    $("#portfolio-projects").append(
+      '<div class="project col-12 col-md-6 col-xl-4 mb-3">' +
+        '<div class="project-body text-center">' +
+          '<h5 class="project-title">' + portfolio[i].name + '</h5>' +
+          '<img class="project-image" src="' + portfolio[i].src + '" alt="' + portfolio[i].alt + '" />' +
+          '<p class="project-skills">' + portfolio[i].skills + '</p>' +
+          '<a class="pagelink btn p-1" href="' + portfolio[i].deployed + '" target="_blank">Deployed Site</a>' +
+          '<a class="pagelink btn p-1" href="' + portfolio[i].github + '" target="_blank">Github Repo</a>' +
+        '</div>' +
+      '</div>'
+    );
   }
 }
 
 /// TECH SKILLS //////////////////////////////////////////////////
+// let techSkillsPage = () => {
+//   for (let i = 0; i < toolbox.length; i++) {
+//     $("#toolbox").append(
+//       // '<div class="toolbox-item header col-6 col-md-4 col-xl-3 p-3">' +
+//       '<div class="toolbox-item">' +
+//         '<h4>' + toolbox[i].name + '</h4>' +
+//         ' <img class="skillimage" src="' + toolbox[i].src + '" alt="' + toolbox[i].alt + '" />' +
+//       '</div>'
+//     )
+//   }
+// }
 let techSkillsPage = () => {
-  for (let i = 0; i < toolbox.length; i++) {
-    $("#toolbox").append(`
-      <div class="toolbox-item header col-6 col-md-4 col-xl-3 p-3">
-        <h4 class="engraved">${toolbox[i].name}</h4>
-        <img class="skillimage" src="${toolbox[i].src}" alt="${toolbox[i].alt}" />
-      </div>
-    `)
-  }
+  toolbox.map(tool => {
+    $("#toolbox").append(
+      '<button type="button" class="toolbox-item" data-toggle="popover" title="'+tool.title+'" data-content="'+tool.content+'">' +
+        ' <img class="skillimage" src="' + tool.src + '" alt="' + tool.alt + '" />' +
+      '</div>'
+    )
+  })
 }
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
--- assets/javascript/toolbox.js
+++ assets/javascript/toolbox.js
@@ -3,14 +3,20 @@ const toolbox = [
     name: "HTML",
     alt: "HTML logo",
     src: "assets/images/html.png",
+    title: "HTML { Proficiency: 10/10 }",
+    content: "Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. HTML describes the structure of a web page.",
   },{
     name: "CSS",
     alt: "CSS logo",
     src: "assets/images/css.png",
+    title: "CSS { Proficiency: 10/10 }",
+    content: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alsongside HTML and JavaScript",
   },{
     name: "JavaScript",
     alt: "JavaScript logo",
     src: "assets/images/javascript.png",
+    title: "JavaScript { Proficiency: 9/10 }",
+    content: "Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it for client-side page behavior, and all major web browsers have a dedicated JavaScript engine to execute it.",
   },{
     name: "jQuery",
     alt: "jQuery logo",
