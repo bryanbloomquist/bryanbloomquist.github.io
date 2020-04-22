45595b1241a739986a32d8794ec3209781094683
Bryan Bloomquist <bmbloomquist@gmail.com>
2020-04-22 10:52:26 -0500

    resized project buttons for mobile

---
+++ assets/javascript/testimonials.js
@@ -0,0 +1,33 @@
+const testimonials = [
+	{
+		name: "Shawn A.",
+		role: "Senior Software Engineer",
+		company: "C.H.Robinson",
+		testimonial: "For my first classroom teaching experience, I wasn't quite sure what to expect from the students. Bryan set the bar right away, for the type of student every teacher hopes for. His eagerness to learn and commitment to the program was apparent from his very first week. Bryan always went the extra mile on his assignments to give them his own creative flair, as a challenge to understand the material. Bryan's work ethic and drive would make him a valuable addition to any development team."
+	},{
+		name: "Ashlen B.",
+		role: "Engineer",
+		company: "Target",
+		testimonial: "Anyone that has the privilege to work with Bryan on a development project will easily see how passionate he is about coding. Even faced with difficult challenges, you can tell Bryan is having fun! His front end design skills stand out, and he easily came up with many creative design concepts for our group projects and his own personal projects. Bryan is also very receptive to feedback, and was able to bring other peoples’ ideas and suggestions to life."
+	},{
+		name: "John E.",
+		role: "Associate Software Engineer",
+		company: "CPSI",
+		testimonial: "I worked on the same team as Bryan on several web applications. He is an incredibly hard-working and and versatile developer who often volunteers for the more difficult project tasks and consistently delivers exceptional results. Bryan's passion for development and technology is apparent through his willingness to always go the extra mile, whether that's single-handedly creating a captivating front-end UI, or diving head-first into a foreign authentication technology. He maintains a quiet intensity throughout the development process, but his easygoing manner and ability to communicate make him a excellent team player. Bryan would be an invaluable asset to any development team."
+	},{
+		name: "Travis H.",
+		role: "Software Engineering Consultant",
+		company: "",
+		testimonial: "Brian was one of my students in Trilogy's Full Stack Web Development program. He quickly excelled to the top of the class, constantly going above and beyond the minimum requirements while also maintaining clean and scalable code. Brian would be a great addition to any development team."
+	},{
+		name: "Scott A.",
+		role: "Support Developer",
+		company: "C.H.Robinson",
+		testimonial: "Throughout my time working with Bryan during a web development boot-camp through the University of Minnesota, I was constantly impressed at his resourcefulness, tenacity, and problem-solving skills. On more than one occasion our team came face to face with a problem that was difficult to solve, only to have Bryan burn the midnight oil and surprise us with an elegant solution. Nothing our team produced would have been possible without Bryan's efforts. His quiet diligence is impressive."
+	},{
+		name: "Steve T.",
+		role: "Principal System Analyst",
+		company: "",
+		testimonial: "I've collaborated with Bryan on multiple projects in the University of Minnesota Coding Boot Camp. His passion for Web Development is reflected in the quality projects that he puts out and the amount of time that he invests into learning each of the technologies and concepts that we learn. He always has a clean front end which makes our classmates, TA's, and the instructor eager to play with his creations. I believe that Bryan has what it takes to be a great Web Developer and would love to work with him in the future!"
+	}
+];
